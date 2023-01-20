734a5c19a9246f77c6192e022d0329817a6f1caf
Beagan Nguy <39427049+beagan-svg@users.noreply.github.com>
2023-01-20 12:51:59 -0800

    Add files via upload

--- sheet_to_text.py
+++ sheet_to_text.py
@@ -1,14 +1,44 @@
+
 import os
 import csv
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+import argparse
 
+# Arg_parser
+def getArgs():
+    # If -x flag is given, script will also produce excel sheet
+    parser = argparse.ArgumentParser(
+        description='Optional arguments: -x', usage='python3 sheet_to_test.py -x'
+    )
+
+    parser.add_argument(
+        '-x', help='Generate Excel Sheet', required=False, action='store_true'
+    )
+
+    parser.add_argument(
+        '-r', help='Remove CSV files generated', required=False, action='store_true'
+    )
+
+    return parser.parse_args()
 
 def main():
-    download_sheet("1zN7iNrgDse61K8ZLzpo7xY4aqOY6CONnEvIQoDLfDLQ")
+    args = getArgs()
+    csvFileNames_list = download_sheet("1zN7iNrgDse61K8ZLzpo7xY4aqOY6CONnEvIQoDLfDLQ")
+    if args.x:
+        # If you want to download in excel format
+        with pd.ExcelWriter(spreadsheet.title + '.xlsx') as writer:
+            for i in range(len(csvFileNames)):
+                read_file = pd.read_csv(csvFileNames[i])
+                newSheetName = csvFileNames[i].split('_', 1)[-1].replace('.csv', '')
+                read_file.to_excel(writer, sheet_name=newSheetName, index=None)
 
+    if args.r:
+        for i in csvFileNames_list:
+            os.remove(i)
 
+    
 def download_sheet(id):
     scope = ['https://spreadsheets.google.com/feeds']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scope)
@@ -28,18 +58,8 @@ def download_sheet(id):
         with open(filename, 'w', encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerows(worksheet.get_all_values())
+    return csvFileNames
 
-    # If you want to download in excel format
-    '''
-    with pd.ExcelWriter(spreadsheet.title + '.xlsx') as writer:
-        for i in range(len(csvFileNames)):
-            read_file = pd.read_csv(csvFileNames[i])
-            newSheetName = csvFileNames[i].split('_', 1)[-1].replace('.csv', '')
-            read_file.to_excel(writer, sheet_name=newSheetName, index=None)
-
-    for i in csvFileNames:
-        os.remove(i)
-    '''
 
 if __name__ == '__main__':
     main()
